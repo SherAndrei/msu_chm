dbe83fe68f38542508cf377506d7e18b44da102a
SherAndrei <welldryus@gmail.com>
2022-10-27 01:38:03 +0300

    second task implemented

--- 02/main.cpp
+++ 02/main.cpp
@@ -2,9 +2,14 @@
 
 #include <cmath>
 #include <cstdio>
+#include <limits>
 
-void E_n(int m, double A);
-double ExactSolution(double A, double h, double k);
+#ifndef m
+#	error Please specify m
+#endif
+
+double E_n(unsigned n, double A);
+double ExactSolution(double A, double x_k);
 void Usage(const char* argv0);
 
 double ExactSolution(double A, double x_k)
@@ -12,106 +17,57 @@ double ExactSolution(double A, double x_k)
 	return std::exp(-A * x_k);
 }
 
-void E_n(int m, double A)
+double E_n(unsigned n, double A)
 {
-	double h = 0.1;
+	const unsigned N = std::pow(10, n);
+	const double h = std::pow(10, -static_cast<double>(n));
+	const double limit = 1e200;
 	double el;
 	double next;
-    double max_e = 0.;
-    double razn = 0.;
-	int n;
-    int N = 10;
-	int fl=0;
+	double max_e = 0.;
 
-    for(n = 1; n <= 3; n++)
-	{
-        for(int k = 0; k<=N; k++)
-		{
-/*            if(h*A >= 1.)
-			{
-                printf("E%d = Err             ", n);
-				fl=1;
-                break;
-			}*/
-
-            Step(k, h, A, el, next);
-
-            if(fabs(next) > 1e200)
-		    {
-                max_e = -1;
-                break;
-			}
-            if(fabs(next) < 1e-100)
-			{
-                max_e = 0;
-                break;
-			}
-            razn = next - ExactSolution(A, (k * h));
-            razn = fabs(razn);
-            if(razn > max_e) max_e = razn;
-		}
-        if(fl==0)printf ("E%d = %e    ", n, max_e);
-        h /= 10;
-        N *= 10;
-		fl=0;
-    }
-
-	h /= 100;
-	N *= 100;
-	n = 6;
-
-	for(int k = 0; k<=N; k++)
-	{
-		/*if(h*A >= 1.)
-		{
-			printf("E%d = Err", n);
-			break;
-		}*/
-
+	for (auto k = 0u; k <= N; k++) {
 		Step(k, h, A, el, next);
-		if(fabs(next) > 1e200)
-		{
+
+		if (std::abs(next) > limit) {
 			max_e = -1.;
 			break;
 		}
-		if(fabs(next) < 1e-100)
-		{
+		if (std::abs(next) < std::numeric_limits<double>::epsilon()) {
 			max_e = 0.;
 			break;
 		}
-		razn = next - exp(-A*(k*h));
-		razn = fabs(razn);
-		if (razn > max_e) max_e = razn;
+		double diff = std::abs(next - ExactSolution(A, (k * h)));
+		if (diff > max_e)
+			max_e = diff;
 	}
-	printf("E%d = %e    ", n, max_e);
-	h /= 10;
-	N *= 10;
-	
-    printf("m = %d    A = %lf, h=%e\n", m, A, h);
+	return max_e;
 }
 
 void Usage(const char* argv0) {
-    std::printf(
-        "Usage: %s m A\n"
-		"\tunsigned m - order of convergence\n"
-        "\tdouble A - task parameter\n"
-        , argv0);
+	std::printf(
+		"Usage: %s A\n"
+		"\tdouble A - task parameter\n"
+		, argv0);
 }
 
 int main(int argc, const char* argv[])
 {
-	if (argc != 3) {
+	if (argc != 2) {
 		Usage(argv[0]);
 		return 1;
 	}
 
 	double A = 0.;
-	unsigned m = 0;
-	if (!(std::sscanf(argv[1], "%lf", &A) == 1
-	   && std::sscanf(argv[1], "%u", &m) == 1)) {
+	if (std::sscanf(argv[1], "%lf", &A) != 1) {
 		std::printf("Error: cannot parse input parameters\n");
 		return 2;
 	}
 
-	E_n(m, A);
+	std::printf("E_1 = %e\t", E_n(1, A));
+	std::printf("E_2 = %e\t", E_n(2, A));
+	std::printf("E_3 = %e\t", E_n(3, A));
+	std::printf("E_6 = %e\t", E_n(6, A));
+	std::printf("m = %u\t", m);
+	std::printf("A = %lf\n", A);
 }
