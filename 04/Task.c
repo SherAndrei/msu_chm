234fcddca552d25f68ba6886f0388a9e87043c30
SherAndrei <welldryus@gmail.com>
2022-12-20 04:39:53 +0300

    04: convert identation to tabs

--- 04/Task.c
+++ 04/Task.c
@@ -4,34 +4,34 @@
 
 static double ExactSolutionValue(double x)
 {
-    return M_PI*x + sin(M_PI*x);
+	return M_PI*x + sin(M_PI*x);
 }
 
 void ExactSolution(double h, double* y, unsigned N)
 {
-    for (unsigned i = 1u; i <= N-1; i++)
-        y[i] = ExactSolutionValue(i * h);
+	for (unsigned i = 1u; i <= N-1; i++)
+		y[i] = ExactSolutionValue(i * h);
 }
 
 static double AddendumValue(double x)
 {
-    (void)x;
-    return 1.;
+	(void)x;
+	return 1.;
 }
 
 void Addendum(double h, double* p, unsigned N)
 {
-    for (unsigned i = 1u; i <= N-1; i++)
-        p[i] = AddendumValue(i * h);
+	for (unsigned i = 1u; i <= N-1; i++)
+		p[i] = AddendumValue(i * h);
 }
 
 static double RightPartValue(double x)
 {
-    return M_PI*M_PI*sin(M_PI*x) + AddendumValue(x)*ExactSolutionValue(x);
+	return M_PI*M_PI*sin(M_PI*x) + AddendumValue(x)*ExactSolutionValue(x);
 }
 
 void RightPart(double h, double* f, unsigned N)
 {
-    for (unsigned i = 1u; i <= N-1; i++)
-        f[i] = RightPartValue(i * h);
+	for (unsigned i = 1u; i <= N-1; i++)
+		f[i] = RightPartValue(i * h);
 }
--- 04/Tridiagonal.c
+++ 04/Tridiagonal.c
@@ -5,44 +5,44 @@
 
 void Solve(const double* P, const double* F, double* Yn, double h, unsigned N)
 {
-    double* const alpha = (double*)calloc(N, sizeof(double));
-    double* const beta = (double*)calloc(N, sizeof(double));
-    const double c1 = 2./(h*h) + P[1];
-    const double b1 = 1./(h*h);
-    const double f1 = F[1];
-    double bk;
-    double ck;
-    double ak;
-    double fk;
-    const double last_c = 1./(h*h)+P[N-1];
-    const double last_a = b1;
+	double* const alpha = (double*)calloc(N, sizeof(double));
+	double* const beta = (double*)calloc(N, sizeof(double));
+	const double c1 = 2./(h*h) + P[1];
+	const double b1 = 1./(h*h);
+	const double f1 = F[1];
+	double bk;
+	double ck;
+	double ak;
+	double fk;
+	const double last_c = 1./(h*h)+P[N-1];
+	const double last_a = b1;
 
-    if (!alpha || !beta) {
-        fprintf(stderr, "Not enought memory for algorithm\n");
-        free(alpha);
-        free(beta);
-        return;
-    }
+	if (!alpha || !beta) {
+		fprintf(stderr, "Not enought memory for algorithm\n");
+		free(alpha);
+		free(beta);
+		return;
+	}
 
-    alpha[2] = b1/c1;
-    beta[2] = f1/c1;
+	alpha[2] = b1/c1;
+	beta[2] = f1/c1;
 
-    for (unsigned k = 2u; k <= N-2; k++)
-    {
-        bk = b1;
-        ck = 2./(h*h)+P[k];
-        ak = b1;
-        fk = F[k];
+	for (unsigned k = 2u; k <= N-2; k++)
+	{
+		bk = b1;
+		ck = 2./(h*h)+P[k];
+		ak = b1;
+		fk = F[k];
 
-        alpha[k+1] = bk/(ck-alpha[k]*ak);
-        beta[k+1]  = (ak*beta[k]+fk)/(ck-alpha[k]*ak);
-    }
+		alpha[k+1] = bk/(ck-alpha[k]*ak);
+		beta[k+1]  = (ak*beta[k]+fk)/(ck-alpha[k]*ak);
+	}
 
-    Yn[N-1] = (F[N-1]+last_a*beta[N-1])/(last_c-last_a*alpha[N-1]);
+	Yn[N-1] = (F[N-1]+last_a*beta[N-1])/(last_c-last_a*alpha[N-1]);
 	
-    for (unsigned k = N - 2; k >= 1; k--)
-        Yn[k] = alpha[k + 1] * Yn[k + 1] + beta[k + 1];
+	for (unsigned k = N - 2; k >= 1; k--)
+		Yn[k] = alpha[k + 1] * Yn[k + 1] + beta[k + 1];
 
-    free(alpha);
-    free(beta);
+	free(alpha);
+	free(beta);
 }
