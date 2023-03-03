d0adb9e48a61274c1005219bf0981daf9fcc10e3
Шерстобитов Андрей Сергеевич <welldryus@gmail.com>
2023-03-03 17:24:50 +0300

    06: start of Vallee-Poussin

--- 06/InterpolationPolynom.c
+++ 06/InterpolationPolynom.c
@@ -6,25 +6,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// from task:
-// * input data consists n+2 points
-// * polynom of degree n so there is n+1 coeff
-
-// by me:
-// * input data constists of N points
-// * polynom of degree N-2 so there is N-1 coeff
-
 static int Usage(const char *argv0, int error) {
-  printf("Usage: %s\n"
-         "\tCalculate interpolation polynom of degree N-2 by using input data which consists N points.\n"
+  printf("Usage: %s m\n"
+         "DESCRIPTION:\n"
+         "\tCalculate interpolation polynom of degree m < (N-1) by using input data which consists N > 1 points.\n"
+	 "OPTIONS:\n"
+	 "\tunsigned m -- degree of desired interpolation polynom\n"
          "\n"
-         "\tExpected input format:\n"
+         "INPUT FORMAT:\n"
          "\t\tN\n"
          "\t\tx1\ty1\n"
          "\t\t...\t...\n"
          "\t\txN\tyN\n"
          "\n"
-         "\tOutput format (N rows):\n"
+         "OUTPUT FORMAT:\n"
+	 "\tN rows, each constists starting nodes, exact solution, result of the interpolation polynom and their difference\n"
          "\t\tx1 \ty1 \tcanonical1\tdelta1\n"
          "\t\tx2 \ty2 \tcanonical2\tdelta2\n"
          "\t\t...\t...\t...\t...\n"
@@ -42,18 +38,18 @@ static void FillVandermondeMatrixAndColumnWithH(const double *x, unsigned N, dou
   }
 }
 
-static int FindCanonicalCoefficients(const double *x, const double *y,
-                                     unsigned N, double *a) {
+static int FindCanonicalCoefficients(const double *x, const double *basis,
+                                     unsigned m, double *a) {
   // last column filled up with (-1)^i
-  double *A = (double *)malloc(N * N * sizeof(double));
+  double *A = (double *)malloc(m * m * sizeof(double));
   if (!A) {
-    fprintf(stderr, "Not enough memory\n");
+    fprintf(stderr, "Not enough memory for matrix\n");
     return 4;
   }
 
-  FillVandermondeMatrixAndColumnWithH(x, N, A);
+  FillVandermondeMatrixAndColumnWithH(x, m, A);
 
-  if (GaussMaxCol(A, N, y, a) < 0) {
+  if (GaussMaxCol(A, m, basis, a) < 0) {
     fprintf(stderr, "Matrix is degenerate!\n");
     free(A);
     return 1;
@@ -87,17 +83,80 @@ static void PrintResult(const double *x, const double *canonical_coefs_with_h,
   fprintf(stdout, "\nh = %e\n", canonical_coefs_with_h[N - 1]);
 }
 
+// from N points select m+2 equally distributed points for basis
+static void SelectBasis(const double* y, unsigned N, unsigned m, double* basis)
+{
+  unsigned step = N / ((m + 2u) - 1u);
+  for (unsigned i = 0u, basis_i = 0u; i < N && basis_i < m; i += step, ++basis_i)
+    basis[basis_i] = y[i];
+}
+
+// if maximum deviation is less than h, then H and H_pos is unchanged
+static void FindMaximumDeviationAndItsPosition(const double* y, const double* canonical_coefs_with_h, unsigned m, double* H, unsigned* H_pos)
+{
+  const double h = canonical_coefs_with_h[m + 1];
+  double current = 0.;
+  for (unsigned i = 0; i < m + 1; ++i) {
+    current = fabs(canonical_coefs_with_h[i] - y[i]); 
+    if (current > h && current > *H) {
+      *H_pos = i;
+      *H = current;
+    }
+  }
+}
+
+static void ValleePoussin(const double *x, const double* y, unsigned N, unsigned m, double* canonical_coefs_with_h)
+{
+  const double eps = 1e-5;
+  double H = 0.;
+  unsigned H_pos = 0u;
+  double h = 0.;
+  // m + 1 coeffs and h at the end
+  double *basis = (double *)malloc((m + 2u) * sizeof(double));
+  if (!basis) {
+    fprintf(stderr, "Not enough memory for Vallee-Poussin algorithm\n");
+    return;
+  }
+
+  do
+  {
+    SelectBasis(y, N, m, basis);
+    FindCanonicalCoefficients(x, basis, m, canonical_coefs_with_h);
+    h = canonical_coefs_with_h[m + 1];
+    FindMaximumDeviationAndItsPosition(y, canonical_coefs_with_h, m, &H, &H_pos);
+    // TODO: adjust basis using algorithm described by kornev
+  } while (h + eps < H);
+
+  free(basis);
+}
+
 int main(int argc, const char *argv[]) {
   unsigned N = 0;
+  unsigned m = 0;
   double *x = NULL;
   double *y = NULL;
   double *canonical_coefs_with_h = NULL;
 
-  if (argc != 1)
+  if (argc != 2)
     return Usage(argv[0], IncorrectUsage);
+
+  if (sscanf(argv[1], "%u", &m) != 1) {
+    fprinrf(stderr, "error: parsing m from args\n");
+    return Usage(argv[0], InputError);
+  }
 
   if (fscanf(stdin, "%u", &N) != 1) {
     fprintf(stderr, "error: parsing N from input\n");
+    return Usage(argv[0], InputError);
+  }
+
+  if (N < 2) {
+    fprintf(stderr, "error: N(=%u) should be greater than 1\n", N);
+    return Usage(argv[0], InputError);
+  }
+
+  if (m >= N - 1) {
+    fprintf(stderr, "error: m(=%u) should be less than N-1(=%u)\n", m, N - 1);
     return Usage(argv[0], InputError);
   }
 
@@ -118,8 +177,9 @@ int main(int argc, const char *argv[]) {
       return Usage(argv[0], InputError);
     }
   }
-  // N-1 coeffs and h at the end
-  canonical_coefs_with_h = (double *)malloc(N * sizeof(double));
+
+  // m + 1 coeffs and h at the end
+  canonical_coefs_with_h = (double *)malloc((m + 2u) * sizeof(double));
   if (!canonical_coefs_with_h) {
     fprintf(stderr, "Not enough memory\n");
     free(x);
@@ -128,7 +188,10 @@ int main(int argc, const char *argv[]) {
     return NotEnoughMemory;
   }
 
-  FindCanonicalCoefficients(x, y, N, canonical_coefs_with_h);
+  if (m == N - 2)
+    FindCanonicalCoefficients(x, y, N, canonical_coefs_with_h);
+  else
+    ValleePoussin(x, y, N, m, canonical_coefs_with_h);
 
   PrintResult(x, canonical_coefs_with_h, N);
 
