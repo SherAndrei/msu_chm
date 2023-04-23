0a6f1c2a12a52688c30cc4934f063a9a13e47136
SherAndrei <welldryus@gmail.com>
2023-04-23 18:22:25 +0300

    09: print min, max and mean of errors

--- 09/print_solution.sh
+++ 09/print_solution.sh
@@ -18,16 +18,20 @@ EOF
 
 [ $# != 3 ] && usage
 
-cat <<EOF | python3
+min_max_mean=$(cat <<EOF | python3
 from math import *;
 
 with open("$1") as inp:
   with open("${1%%/*}/errors.txt", "w") as errors:
+    all_errors = []
     for line in inp.readlines():
       x, y, val = map(float, line.split())
       error = abs(val - $2)
+      all_errors.append(error)
       errors.write(f"{x} {y} {error}\n")
+    print("min:", min(all_errors), "max:", max(all_errors), "mean:", sum(all_errors) / len(all_errors))
 EOF
+)
 
 cat <<EOF | gnuplot
 set terminal png size 1600,800
@@ -44,6 +48,7 @@ set label 1
 unset label 1
 
 set label 2
+  set title "$min_max_mean"
   splot "${1%%/*}/errors.txt" title 'Errors' lw 2
 unset label 2
 
