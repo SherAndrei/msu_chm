69d70631181e0214abad967bd5813e925fb22da1
SherAndrei <welldryus@gmail.com>
2023-04-23 22:22:34 +0300

    09: sew name of solution into error filename in print_solution.h

--- 09/print_solution.sh
+++ 09/print_solution.sh
@@ -18,11 +18,13 @@ EOF
 
 [ $# != 3 ] && usage
 
+error_filename="${1%%.*}_errors.txt"
+
 min_max_mean=$(cat <<EOF | python3
 from math import *;
 
 with open("$1") as inp:
-  with open("${1%%/*}/errors.txt", "w") as errors:
+  with open("$error_filename", "w") as errors:
     all_errors = []
     for line in inp.readlines():
       x, y, val = map(float, line.split())
@@ -49,7 +51,7 @@ unset label 1
 
 set label 2
   set title "$min_max_mean"
-  splot "${1%%/*}/errors.txt" title 'Errors' lw 2
+  splot "$error_filename" title 'Errors' lw 2
 unset label 2
 
 unset multiplot
